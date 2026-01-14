232d85a173c3220eafbfa7551cb5ee0b97f69dc7
Jonah Kember <jonah.kember@mail.mcgill.ca>
2026-01-14 16:32:20 -0500

    modified main

---
+++ README.md
@@ -0,0 +1,9 @@
+Useage:
+
+### Create/activate virtual environment, install dependencies
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+### Edit config.json with appropriate paths
+python main.py --config config.json
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 import json
+import shutil
 import numpy as np
 import argparse
 import utils
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config')
-args = parser.parse_args()
+def main():
 
-with open(str(args.config)) as f:
-    cfg = json.load(f)
+    if shutil.which('wb_command') is None:
+        raise RuntimeError('wb_command not found. Please install Connectome Workbench and add to your path.')
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config')
+    args = parser.parse_args()
 
-if __name__ == '__main__':
+    with open(str(args.config)) as f:
+        cfg = json.load(f)
 
     output   = cfg['output']
     subjects = np.loadtxt(f"{cfg['subjects']}", dtype=str)
@@ -22,5 +25,9 @@ if __name__ == '__main__':
         if os.path.exists(f'{output}/data/sub-{subject}/networks.R.label.gii'):
             continue
 
-        # utils.clean_fmriprep_output(cfg, subject)
+        utils.clean_fmriprep_output(cfg, subject)
         utils.run_precision_mapping(cfg, subject)
+
+
+if __name__ == '__main__':
+    main()
